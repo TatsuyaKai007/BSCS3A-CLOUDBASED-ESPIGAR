133559eb2b28b4aa3089c07fa9cbe6cfaf2f6caa
TatsuyaKai007 <espigarjoierian@gmail.com>
2026-08-14 13:41:15 +0800

    Added Contact.php and updated Art.php

---
+++ Espigar-Final-Bio/Contact.php
@@ -0,0 +1,35 @@
+<?php
+    $title = "My Biography";
+    $headerName = "Contact Information and Socials";
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $headerName; ?></h1>
+        <br>
+       <ul>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjmUvHck4ejf1Lg99_HEImTZnwtlCG9rLKolFWzHzJJg&s=10" alt="Facebook
+            " width="150">
+            <li><a href="https://www.facebook.com/EspigarJoieRian">Facebook: Joie Rian Espigar</a></li>
+            <br>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUXfUL28oZ5EfH8SYzjwtutOTCD9iQrU8rszuNXKtawg&s=10" alt="Gmail>" width="150">
+            <li><a href="https://mail.google.com/mail/u/0/#inbox">gmail: espigarjoierian@gmail.com</a></li>
+            <br>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr-pU7BmEmCVcRrteVag3G7XAJ0m01V_41LJgLi116fA&s" alt="Phone" width="150">
+            <li>Phone: 09128088009</li>
+        <br><br>
+        <nav>
+            <a href="bio.php">Biography</a>
+            <br>
+            <a href="Art.php">Artworks</a>
+        </nav>
+   
+    </body>  
+</html>
