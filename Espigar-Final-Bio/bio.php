acd8ec6e1f7b97a1339dd773e5a531ce310cbd2c
TatsuyaKai007 <espigarjoierian@gmail.com>
2026-08-14 13:57:34 +0800

    This is the Final Folder for my submission Sir

--- Espigar-Final-Bio/bio.php
+++ Espigar-Final-Bio/bio.php
@@ -13,7 +13,7 @@
     <body>
         <h1><?php echo $headerName; ?></h1>
         <br>
-        <p>My name is Joie Rian Espigar, and I am currently a Computer Science student in University of St. La Salle. I aspire to be a Web developer or a Software Engineer. I love to draw, sing, play video games, and play different types of sports like basketball, volleyball, badminton, and many more. Just to add, I love to eat Sisig, Baked Mac and Cheese, Cheeseburger, and any kind of pasta, and Caesar Salad. That is all I want to share right now and I hope to share more in the future.</p>
+        <p>My name is Joie Rian Espigar, and I am currently a Computer Science student in University of St. La Salle. I aspire to be a Web developer or a Software Engineer. I love to draw, sing, play video games, and play different types of sports like basketball, volleyball, badminton, and many more. Just to add, I love to different kinds of food like Sisig, Baked Mac and Cheese, Cheeseburger, and any kind of pasta, and Caesar Salad. That is all I want to share right now and I hope to share more in the future.</p>
         <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/509441855_2825134294346058_5301453185011270330_n.jpg?stp=dst-jpg_tt6&cstp=mx1536x1541&ctp=s1536x1541&_nc_cat=100&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeEv_Cg7Ik1Wy6kEE93JizLZM3EjSrQGJLgzcSNKtAYkuNa1NYW2VDytXTYNbgzxeMtQOP4qXU4k7DzhZO06aMFH&_nc_ohc=sjQ81ecWxCYQ7kNvwFERCqN&_nc_oc=AdogNb3y-_TsFfZDERyFwOsE5hbQBxyWETA_p0PPDEn1bAV7WaFkBKR3WzcYhWo4yRyqjvxMtp3_sKtd-xdAF-Rk&_nc_zt=23&_nc_ht=scontent.fceb2-1.fna&_nc_gid=UMo8BaTyL5GxPDU9tLak4w&_nc_ss=7b2a8&oh=00_AQE-Wv5MS3Tyo4GXQTHni3jsPJi4lU351-6R4BhdpqNa_Q&oe=6A83D476" alt="Joie Rian Espigar" width="300">
         <br><br>
         <nav>
